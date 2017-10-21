12027b8fd9a327145434582e9ed2dc02968379da
James Ray <jray@five-startech.com>
2017-10-20 22:15:51 -0400

    Add flowchart and my first project files to repo

---
+++ MyFirstProject/src/Introduction.java
@@ -0,0 +1,20 @@
+
+/**
+ * Program: Introduction
+ * File: Introduction.java
+ * Summary: Introduces me by printing out content to console.
+ * Author: James Ray
+ * Date: October 20, 2017
+ */
+public class Introduction {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("My name is James Ray.");
+        System.out.println("My favorite color is Red.");
+        System.out.println("If I won the lottery, I would finally be able to take care of my family the way I hope I can someday.");
+    }
+
+}
